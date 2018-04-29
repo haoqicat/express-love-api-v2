2af2569be1512aec72b015ade2d4686f3d1f7ae4
Peter Wang <happypeter1983@gmail.com>
2018-04-29 10:13:12 +0800

    3-mongoose

--- api/index.js
+++ api/index.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
 
+// mongoose START
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/express-love-api')
+const db = mongoose.connection
+db.on('error', () => console.error('Mongo Failed to Connect!!!!'))
+db.on('connected', () => console.log('Mongo Connected'))
+// mongoose END
+
+const Post = require('./models/post')
+
+app.post('/post', () => {
+  const post = new Post({ title: 'mogoose usage' })
+  post.save()
+})
+
 app.get('/', (req, res) => {
   res.send('Hello Client')
 })
