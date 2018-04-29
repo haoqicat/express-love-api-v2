1c11294972f0a0a190e1581e9e72083e02551d4f
Peter Wang <happypeter1983@gmail.com>
2018-04-29 10:25:56 +0800

    4-api

--- api/index.js
+++ api/index.js
@@ -11,9 +11,58 @@ db.on('connected', () => console.log('Mongo Connected'))
 
 const Post = require('./models/post')
 
-app.post('/post', () => {
-  const post = new Post({ title: 'mogoose usage' })
-  post.save()
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+app.post('/post', async (req, res) => {
+  const p = new Post(req.body)
+  try {
+    const post = await p.save()
+    res.json(post)
+  } catch (err) {
+    res.status(500).json({ msg: '保存失败', err })
+  }
+})
+
+app.get('/posts', async (req, res) => {
+  try {
+    const posts = await Post.find()
+    res.json(posts)
+  } catch (err) {
+    res.status(500).json({ msg: '读取失败', err })
+  }
+})
+
+app.get('/post/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    res.json(post)
+  } catch (err) {
+    res.status(500).json({ msg: '读取失败', err })
+  }
+})
+
+app.put('/post/:id', async (req, res) => {
+  try {
+    const p = await Post.findById(req.params.id)
+    for (prop in req.body) {
+      p[prop] = req.body[prop]
+    }
+    const post = await p.save()
+    res.json(post)
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+})
+
+app.delete('/post/:id', async (req, res) => {
+  try {
+    const p = await Post.findById(req.params.id)
+    const post = await p.remove()
+    res.json(post)
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
 })
 
 app.get('/', (req, res) => {
